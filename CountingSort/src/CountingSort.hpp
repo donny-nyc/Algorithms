260a2dbf7dc33714dcb35dd0d5b7d25d5d248ebb
Donny <mail@donny.nyc>
2021-08-26 01:21:06 -0400

    wip: counting sort works with simple example

--- CountingSort/src/CountingSort.hpp
+++ CountingSort/src/CountingSort.hpp
@@ -3,19 +3,25 @@
  * derived class. No params are passed
  * during the sort() call itself
  */
+template <typename T>
 class SortStrategy {
     public:
-        virtual void sort() = 0;
+        virtual T* sort() = 0;
 };
 
 template <typename T>
-class CountingSort : public SortStrategy {
+class CountingSort : public SortStrategy<T> {
     private:
         T* records;
+        int count = 0;
+        int max = 0;
     public:
         CountingSort<T>();
-        void sort();
-        void setRecords(T* r) { records = r; }
+        CountingSort<T>(T* r, int m, int c): records(r), max(m), count(c) {};
+        int getMax() { return max; }
+        int size() { return count; }
+        T* sort();
+        void setRecords(T* r, int m, int c) { records = r; max = m; count = c; }
 };
 
 #include "CountingSort.tpp"
--- CountingSort/src/test.cpp
+++ CountingSort/src/test.cpp
@@ -6,7 +6,16 @@ using namespace std;
 
 int main() {
     //CountingSort<int> sort = CountingSort<int>();
-    CountingSort<int> sort = CountingSort<int>();
+    int records[] = {4, 6, 1, 2, 8};
+    CountingSort<int> sort = CountingSort<int>(records, 8, 5);
 
-    sort.sort();
+    int* results = sort.sort();
+
+    for(int j = 0; j < sort.size(); j++) {
+        cout << results[j] << ", ";
+    }
+
+    cout << endl;
+
+    return 0;
 }
